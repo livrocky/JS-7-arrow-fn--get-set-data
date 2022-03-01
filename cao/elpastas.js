57f601622728c8b95751bb08db086967db7bf076
rokas <rokasra@gmail.com>
2022-03-01 17:35:01 +0200

    cao 3 uzd done

---
+++ cao/elpastas.js
@@ -0,0 +1,1 @@
+//Sukurk programą, kurioje reikia įvesti elektroninį paštą ir paspausti mygtuką "sutinku gauti laiškus". Suvedus duomenis, jei mygtukas "sutinku" nepasirinktas - išmeta - "Registracija nesėkminga. Jei pasirinktas - išmeta "El. paštas [paštas] sėkmingai užregistruotas".
