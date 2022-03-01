765ebeecf8589b6dcd5d43d8ce92a1c399fcf6e3
rokas <rokasra@gmail.com>
2022-03-01 15:18:25 +0200

    uzd3 prideta

--- js/uzd3.js
+++ js/uzd3.js
@@ -6,9 +6,7 @@ function fn() {
   const elH1 = document.querySelector("h1");
   const elP = document.getElementById("result");
   const result = (elH1.innerHTML = elP.innerHTML);
-  console.log(result);
+  //   console.log(result);
   return result;
-
-  //NUSITAIKYK elP taisyklingai, uzidek id. verta kurtis nauja html.
 }
 fn();
